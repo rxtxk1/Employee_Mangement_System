7a975ee750457a086599ec68fe4c700040f5a812
rxtxk <ritiksinghz746@gmail.com>
2025-06-05 00:38:24 +0530

    leave functionalaty done with error in employee profile in fetching leave data

--- server/controllers/leaveController.js
+++ server/controllers/leaveController.js
@@ -1,122 +1,42 @@
-// import path from 'path';
-// import Employee from '../models/Employee.js';
-// import Leave from '../models/Leave.js';
-
-// const addLeave = async (req, res) => {
-//     try {
-//   const { userId, leaveType, startDate, endDate, reason} = req.body;
-//   const employee = await Employee.findOne({ userId });
-
-//   const newLeave = new Leave({
-//     employeeId: employee._id, leaveType, startDate, endDate, reason
-//   });
-
-// await newLeave.save();
-
-//   return res.status(200).json({ success: true});
-
-// } catch (error) {
-//   return res.status(500).json({ success: false, error: 'leave add server error' });
-// }
-
-// }
-
-// const getLeave = async (req, res) => {
-//   try {
-//     const {id} = req.params;
-//     const employee = await Employee.findOne({ userId: id });
-
-//     const leaves = await Leave.find({ employeeId: employee._id });
-//     return res.status(200).json({ success: true, leaves });
-
-//   }catch (error) {
-//     console.error(error.message);
-//     return res.status(500).json({ success: false, error: 'leave add server error' });
-//   }
-// }
-
-// const getLeaves = async (req, res) => {
-//   try {
-//     const leaves = await Leave.find().populate({
-//       path: 'employeeId',
-//       populate: [
-//         {
-//               path: 'department',
-//               select: 'dep_name'
-//         },
-//         {
-//               path: 'userId',
-//               select: 'name'
-//         },
-//       ]
-//     })
-
-//     return res.status(200).json({ success: true, leaves });
-
-//   }catch (error) {
-//     console.error(error.message);
-//     return res.status(500).json({ success: false, error: 'leave add server error' });
-//   }
-// }
-
-// export {addLeave, getLeave, getLeaves};
-
-
-
 import path from 'path';
 import Employee from '../models/Employee.js';
 import Leave from '../models/Leave.js';
-import { profile } from 'console';
 
 const addLeave = async (req, res) => {
-  try {
-    const { userId, leaveType, startDate, endDate, reason } = req.body;
-    const employee = await Employee.findOne({ userId });
+    try {
+  const { userId, leaveType, startDate, endDate, reason} = req.body;
+  const employee = await Employee.findOne({ userId });
 
-    const newLeave = new Leave({
-      employeeId: employee._id,
-      leaveType,
-      startDate,
-      endDate,
-      reason,
-    });
+  const newLeave = new Leave({
+    employeeId: employee._id, leaveType, startDate, endDate, reason
+  });
 
-    await newLeave.save();
+await newLeave.save();
 
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    return res.status(500).json({ success: false, error: 'leave add server error' });
-  }
-};
+  return res.status(200).json({ success: true});
+
+} catch (error) {
+  return res.status(500).json({ success: false, error: 'leave add server error' });
+}
+
+}
 
 const getLeave = async (req, res) => {
   try {
-    const { id } = req.params;
-
-    const leave = await Leave.findById(id).populate({
-      path: 'employeeId',
-      populate: [
-        {
-          path: 'department',
-          select: 'dep_name',
-        },
-        {
-          path: 'userId',
-          select: 'name profileImage dob gender maritalStatus',
-        },
-      ],
-    });
-
-    if (!leave) {
-      return res.status(404).json({ success: false, error: 'Leave not found' });
+    const {id} = req.params;
+    let leaves = await Leave.find({ employeeId: id })
+    if (!leaves) {
+        const employee = await Employee.findOne({ userId: id });
+        leaves = await Leave.find({ employeeId: employee._id });
     }
+    
+    return res.status(200).json({ success: true, leaves });
 
-    return res.status(200).json({ success: true, leave });
-  } catch (error) {
+  }catch (error) {
     console.error(error.message);
-    return res.status(500).json({ success: false, error: 'Server error' });
+    return res.status(500).json({ success: false, error: 'leave add server error' });
   }
-};
+}
 
 const getLeaves = async (req, res) => {
   try {
@@ -124,27 +44,28 @@ const getLeaves = async (req, res) => {
       path: 'employeeId',
       populate: [
         {
-          path: 'department',
-          select: 'dep_name',
+              path: 'department',
+              select: 'dep_name'
         },
         {
-          path: 'userId',
-          select: 'name',
+              path: 'userId',
+              select: 'name'
         },
-      ],
-    });
+      ]
+    })
 
     return res.status(200).json({ success: true, leaves });
-  } catch (error) {
+
+  }catch (error) {
     console.error(error.message);
     return res.status(500).json({ success: false, error: 'leave add server error' });
   }
-};
+}
 
 const getLeaveDetail = async (req, res) => {
   try {
     const {id} = req.params;
-    const leave = await Leave.findById().populate({
+    const leave = await Leave.findById(id).populate({
       path: 'employeeId',
       populate: [
         {
@@ -166,5 +87,141 @@ const getLeaveDetail = async (req, res) => {
 
 };
 
+const updateLeave = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const leave = await Leave.findByIdAndUpdate({_id: id}, {status: req.body.status});
+    if (!leave) {
+      return res.status(404).json({ success: false, error: 'Leave not found' });
+    }
 
-export { addLeave, getLeave, getLeaves, getLeaveDetail };
+    return res.status(200).json({ success: true});
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ success: false, error: 'leave update Server error' });
+  }
+};
+
+export {addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave};
+
+
+
+// import path from 'path';
+// import Employee from '../models/Employee.js';
+// import Leave from '../models/Leave.js';
+// import { profile } from 'console';
+
+// const addLeave = async (req, res) => {
+//   try {
+//     const { userId, leaveType, startDate, endDate, reason } = req.body;
+//     const employee = await Employee.findOne({ userId });
+
+//     const newLeave = new Leave({
+//       employeeId: employee._id,
+//       leaveType,
+//       startDate,
+//       endDate,
+//       reason,
+//     });
+
+//     await newLeave.save();
+
+//     return res.status(200).json({ success: true });
+//   } catch (error) {
+//     return res.status(500).json({ success: false, error: 'leave add server error' });
+//   }
+// };
+
+// const getLeave = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+
+//     const leave = await Leave.findById(id).populate({
+//       path: 'employeeId',
+//       populate: [
+//         {
+//           path: 'department',
+//           select: 'dep_name',
+//         },
+//         {
+//           path: 'userId',
+//           select: 'name profileImage dob gender maritalStatus',
+//         },
+//       ],
+//     });
+
+//     if (!leave) {
+//       return res.status(404).json({ success: false, error: 'Leave not found' });
+//     }
+
+//     return res.status(200).json({ success: true, leave });
+//   } catch (error) {
+//     console.error(error.message);
+//     return res.status(500).json({ success: false, error: 'Server error' });
+//   }
+// };
+
+// const getLeaves = async (req, res) => {
+//   try {
+//     const leaves = await Leave.find().populate({
+//       path: 'employeeId',
+//       populate: [
+//         {
+//           path: 'department',
+//           select: 'dep_name',
+//         },
+//         {
+//           path: 'userId',
+//           select: 'name',
+//         },
+//       ],
+//     });
+
+//     return res.status(200).json({ success: true, leaves });
+//   } catch (error) {
+//     console.error(error.message);
+//     return res.status(500).json({ success: false, error: 'leave add server error' });
+//   }
+// };
+
+// const getLeaveDetail = async (req, res) => {
+//   try {
+//     const {id} = req.params;
+//     const leave = await Leave.findById().populate({
+//       path: 'employeeId',
+//       populate: [
+//         {
+//           path: 'department',
+//           select: 'dep_name',
+//         },
+//         {
+//           path: 'userId',
+//           select: 'name, profileImage'
+//         },
+//       ],
+//     });
+
+//     return res.status(200).json({ success: true, leave });
+//   } catch (error) {
+//     console.error(error.message);
+//     return res.status(500).json({ success: false, error: 'leave add server error' });
+//   }
+
+// };
+
+// const updateLeave = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const leave = await Leave.findByIdAndUpdate({_id: id}, {status: req.body.status});
+//     if (!leave) {
+//       return res.status(404).json({ success: false, error: 'Leave not found' });
+//     }
+
+//     return res.status(200).json({ success: true});
+//   } catch (error) {
+//     console.error(error.message);
+//     return res.status(500).json({ success: false, error: 'leave update Server error' });
+//   }
+// };
+
+// export { addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave };
