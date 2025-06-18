a1ec7bd635500ded97caaa51ced35a2ea7c570a5
rxtxk <ritiksinghz746@gmail.com>
2025-06-18 13:23:57 +0530

    error fixed

--- server/controllers/leaveController.js
+++ server/controllers/leaveController.js
@@ -23,9 +23,12 @@ await newLeave.save();
 
 const getLeave = async (req, res) => {
   try {
-    const {id} = req.params;
-    let leaves = await Leave.find({ employeeId: id })
-    if (!leaves || leaves.length === 0) {
+    const {id, role} = req.params;
+    let leaves
+    if (role === 'admin') {
+      leaves = await Leave.find({ employeeId: id })
+
+    } else {
         const employee = await Employee.findOne({ userId: id });
         leaves = await Leave.find({ employeeId: employee._id });
     }
@@ -74,7 +77,7 @@ const getLeaveDetail = async (req, res) => {
         },
         {
           path: 'userId',
-          select: 'name, profileImage'
+          select: 'name profileImage'
         },
       ],
     });
@@ -82,7 +85,7 @@ const getLeaveDetail = async (req, res) => {
     return res.status(200).json({ success: true, leave });
   } catch (error) {
     console.error(error.message);
-    return res.status(500).json({ success: false, error: 'leave add server error' });
+    return res.status(500).json({ success: false, error: 'leave detail server error' });
   }
 
 };
--- server/routes/leave.js
+++ server/routes/leave.js
@@ -6,8 +6,8 @@ import { addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave } from '../c
 const router = express.Router()
 
 router.post('/add', authMiddleware, addLeave)
-router.get('/:id', authMiddleware, getLeave)
 router.get('/detail/:id', authMiddleware, getLeaveDetail)
+router.get('/:id/:role', authMiddleware, getLeave)
 router.get('/', authMiddleware, getLeaves) // For admin to get all leaves
 router.put('/:id', authMiddleware, updateLeave)
 
